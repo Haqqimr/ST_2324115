64a552c701b0dab8cd80638e388a7cedf0f7f396
Haqqimr <148042829+Haqqimr@users.noreply.github.com>
2024-04-18 18:50:21 +0800

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,15 @@
+# ini pembelajaran Variabel
+e = 9
+i = 4
+haqi = 3
+haqi_ganteng = 100
+kata10 = 10
+print("nilai sy adalah =",e)
+print("nilai sy adalah =",i)
+print("nilai sy adalah =",haqi)
+print("nilai sy adalah =",haqi_ganteng)
+#intup kedua
+e = 6
+print("nilai sy adalah =",e)
+z = e
+print("nilai sy adalah =",z)
